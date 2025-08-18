916c5c209acc155410951535438f66d3f8cbb3cf
DELLOSA-coder <24.jpad@gmail.com>
2025-08-18 17:20:28 +0800

    Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,11 @@
+a = 9
+b = 8
+
+c = 8
+d = 9
+username = 'majimboss'
+password = 'bossing10'
+print( username == password )
+print( (a > b) and (c < d) )
+print( (a > b) or (c < d) )
+print(not((a > b) or (c < d)))
