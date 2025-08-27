bf5df4ba668f68ea88a0d2e249d338dda95116d5
DELLOSA-coder <24.jpad@gmail.com>
2025-08-27 15:46:54 +0800

    Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,19 @@
+username = "Akomabaho"
+
+password = "Araykoh"
+
+
+uname = input("Enter username -->")
+pword = input("Enter password -->")
+
+
+
+
+if(uname == username) and (pword == password):
+
+
+     print(" ACCESS GRANTED ")
+
+else:
+
+     print(" ACCESS GRANTED ")
