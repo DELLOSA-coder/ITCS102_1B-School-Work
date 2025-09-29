d08c7e6b20e810266bb29d61b23bd72a34945d95
DELLOSA-coder <24.jpad@gmail.com>
2025-09-29 18:37:13 +0800

    Nest

--- activity19.py
+++ activity19.py
@@ -5,7 +5,8 @@
 
 for i in range(1,11,1):
     for x in range(1,i,1):
-        print("🌚",end=" ")
+        print("😈",end=" ")
     for y in range(10,i,-1):
-        print("🌞",end=" ")
+        print("😇",end=" ")
+
     print()
